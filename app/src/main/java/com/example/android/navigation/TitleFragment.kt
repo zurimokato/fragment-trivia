ff7d1fa9d7066a2270ee480777be90ddb11b00bf
zurimokato <noj2304@gmail.com>
2021-08-20 18:25:50 -0500

    addging navigation

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,14 +7,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
 class TitleFragment : Fragment() {
-
-
 
 
     @SuppressLint("SetTextI18n")
@@ -23,7 +22,10 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-       val binding:FragmentTitleBinding=DataBindingUtil.inflate(inflater,R.layout.fragment_title,container, false)
+        val binding: FragmentTitleBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        binding.playButton.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
         return binding.root
     }
 
